ef598ea5dacd3828ad23c002be4d210299cbfe64
Jinseong <likemath1997@naver.com>
2025-06-27 22:46:28 +0900

    fix: remove padding and add Space

--- friends_event/src/App.tsx
+++ friends_event/src/App.tsx
@@ -6,19 +6,19 @@ import End from "./pages/End";
 import NotFound from "./pages/NotFound";
 import Game from "./pages/Game";
 import Rank from "./pages/Rank";
+import Space from "./components/Space";
 
 function App() {
   return (
     <div
       css={{
-        position: "relative",
-        padding: "40px 0px 25px 0px",
         height: "100%",
         boxSizing: "border-box",
         display: "flex",
         flexDirection: "column",
       }}
     >
+      <Space height={40} />
       <Routes>
         <Route path="/" element={<Event />} />
         <Route path="/game" element={<Game />} />
@@ -26,6 +26,7 @@ function App() {
         <Route path="/end" element={<End />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Space height={25} />
     </div>
   );
 }
